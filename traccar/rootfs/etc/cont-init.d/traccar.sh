537f8405fb84f6797951584358a8f0706694b197
Mikescotland <52081323+Mikescotland@users.noreply.github.com>
2026-04-21 14:35:42 +0200

    Update traccar.sh

--- traccar/rootfs/etc/cont-init.d/traccar.sh
+++ traccar/rootfs/etc/cont-init.d/traccar.sh
@@ -20,15 +20,5 @@ if ! bashio::fs.file_exists "/config/traccar.xml"; then
     cp /etc/traccar/traccar.xml /config/traccar.xml
 else
     # Existing installation
-    if bashio::services.available "mysql"; then
-        # Make sure the database isn't locked
-        host=$(bashio::services "mysql" "host")
-        password=$(bashio::services "mysql" "password")
-        port=$(bashio::services "mysql" "port")
-        username=$(bashio::services "mysql" "username")
-
-        echo "UPDATE DATABASECHANGELOGLOCK SET locked=0;" \
-            | mysql --skip-ssl -h "${host}" -P "${port}" -u "${username}" -p"${password}" \
-                traccar || true
-    fi
+    
 fi
