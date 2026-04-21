5645d288a9eddf250b17309bef92f9ef50874f8c
Mikescotland <52081323+Mikescotland@users.noreply.github.com>
2026-04-21 14:49:43 +0200

    Update traccar.sh

--- traccar/rootfs/etc/cont-init.d/traccar.sh
+++ traccar/rootfs/etc/cont-init.d/traccar.sh
@@ -3,22 +3,17 @@
 # Home Assistant Community Add-on: Traccar
 # Ensures the user configuration file is present
 # ==============================================================================
-declare host
-declare password
-declare port
-declare username
 
 # Migrate add-on data from the Home Assistant config folder,
 # to the add-on configuration folder.
-if ! bashio::fs.directory_exists '/config/traccar.xml' \
+if ! bashio::fs.file_exists '/config/traccar.xml' \
     && bashio::fs.file_exists '/homeassistant/traccar.xml'; then
     mv /homeassistant/traccar.xml /config/traccar.xml \
         || bashio::exit.nok "Failed to migrate Traccar configuration"
 fi
 
+# Create default config on first start
 if ! bashio::fs.file_exists "/config/traccar.xml"; then
-    cp /etc/traccar/traccar.xml /config/traccar.xml
-else
-    # Existing installation
-    
+    cp /etc/traccar/traccar.xml /config/traccar.xml \
+        || bashio::exit.nok "Failed to create default Traccar configuration"
 fi
